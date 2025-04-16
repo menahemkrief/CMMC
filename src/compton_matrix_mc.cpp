2edfd40111ae2c92ce8e6ac8582efa021d7f8e3c
ItamarShmelo <itamargiron@gmail.com>
2025-04-16 12:46:44 +0300

    reformating

--- src/compton_matrix_mc.cpp
+++ src/compton_matrix_mc.cpp
@@ -10,67 +10,68 @@
 
 static double constexpr signaling_NaN = std::numeric_limits<double>::signaling_NaN();
 
-ComptonMatrixMC::ComptonMatrixMC(Vector const energy_groups_centers_, 
-                                 Vector const energy_groups_boundries_, 
-                                 std::size_t const num_of_samples_, 
-                                 bool const force_detailed_balance_,
-                                 int const seed_) :
-                            energy_groups_centers(energy_groups_centers_),
-                            energy_groups_boundries(energy_groups_boundries_),
-                            num_energy_groups(energy_groups_centers.size()),
-                            num_of_samples(num_of_samples_), 
-                            seed(seed_ >= 0 ? seed_ : static_cast<unsigned int>(std::time(0))),
-                            sample_uniform_01(
-                                boost::random::mt19937(seed),
-                                boost::random::uniform_01<>()
-                            ),
-                            force_detailed_balance(force_detailed_balance_),
-                            temperature_grid(),
-                            S_log_tables(),
-                            dSdUm_tables(),
-                            S_temp(num_energy_groups, Vector(num_energy_groups, signaling_NaN)),
-                            n_eq(num_energy_groups, signaling_NaN),
-                            B(num_energy_groups, signaling_NaN) {
+ComptonMatrixMC::ComptonMatrixMC(
+    Vector const energy_groups_centers_,
+    Vector const energy_groups_boundries_,
+    std::size_t const num_of_samples_,
+    bool const force_detailed_balance_,
+    int const seed_) :
+    energy_groups_centers(energy_groups_centers_),
+    energy_groups_boundries(energy_groups_boundries_),
+    num_energy_groups(energy_groups_centers.size()),
+    num_of_samples(num_of_samples_),
+    seed(seed_ >= 0 ? seed_ : static_cast<unsigned int>(std::time(0))),
+    sample_uniform_01(
+        boost::random::mt19937(seed),
+        boost::random::uniform_01<>()
+    ),
+    force_detailed_balance(force_detailed_balance_),
+    temperature_grid(),
+    S_log_tables(),
+    dSdUm_tables(),
+    S_temp(num_energy_groups, Vector(num_energy_groups, signaling_NaN)),
+    n_eq(num_energy_groups, signaling_NaN),
+    B(num_energy_groups, signaling_NaN) {
     printf("Generating a ComptonMatrixMC object... seed=%d\n", seed);
-    if(num_energy_groups + 1 != energy_groups_boundries.size()){
+    if (num_energy_groups + 1 != energy_groups_boundries.size()) {
         printf("ComptonMatrixMC fatal - inconsistent number of energy group boundaries and centers\n");
         exit(1);
     }
-    for(std::size_t g=0; g<num_energy_groups; ++g){
-        if(energy_groups_boundries[g]   < 0. or
-           energy_groups_boundries[g]   >= energy_groups_boundries[g+1] or
-           energy_groups_boundries[g]   >= energy_groups_centers[g] or
-           energy_groups_boundries[g+1] <= energy_groups_centers[g]){
+    for (std::size_t g=0; g<num_energy_groups; ++g) {
+        if (energy_groups_boundries[g]   < 0. or
+            energy_groups_boundries[g]   >= energy_groups_boundries[g+1] or
+            energy_groups_boundries[g]   >= energy_groups_centers[g] or
+            energy_groups_boundries[g+1] <= energy_groups_centers[g]) {
             printf("ComptonMatrixMC fatal - inconsistent energy group boundaries/centers\n");
             exit(1);
         }
     }
 
     printf("Compton matrices defined on %ld groups.\nPhoton energy group boundaries (in kev) \n", num_energy_groups);
-    for(auto const e : energy_groups_boundries){
+    for (auto const e : energy_groups_boundries) {
         printf("%g ", e/units::kev);
     }
-    printf("\n");    
+    printf("\n");
 }
 
-double ComptonMatrixMC::sample_gamma(double const temperature){
-    
+double ComptonMatrixMC::sample_gamma(double const temperature) {
+
     double const theta = units::k_boltz * temperature / units::me_c2;
     double const sum_1_bt = 1.0 + 1.0 / theta;
-    double const Sb = sum_1_bt + 0.5/(theta*theta); 
+    double const Sb = sum_1_bt + 0.5/(theta*theta);
 
     double const r0Sb = sample_uniform_01()*Sb;
-    
+
     double const r1 = sample_uniform_01();
 
-    if(r0Sb <= 1.0){
+    if (r0Sb <= 1.0) {
         double const r2 = sample_uniform_01();
         double const r3 = sample_uniform_01();
 
         return 1.0 - theta*std::log(r1*r2*r3);
     }
 
-    if(r0Sb <= sum_1_bt){
+    if (r0Sb <= sum_1_bt) {
         double const r2 = sample_uniform_01();
 
         return 1.0 - theta*std::log(r1*r2);
@@ -79,10 +80,10 @@ double ComptonMatrixMC::sample_gamma(double const temperature){
     return 1.0 - theta*std::log(r1);
 }
 
-void ComptonMatrixMC::set_Bg_ng(double const temperature){
+void ComptonMatrixMC::set_Bg_ng(double const temperature) {
     using boost::math::pow;
     double constexpr fac = pow<3>(units::clight) / (8.0*M_PI*units::planck_constant);
-    for(std::size_t g=0; g < num_energy_groups; ++g){
+    for (std::size_t g=0; g < num_energy_groups; ++g) {
         double const Bg = planck_integral::planck_energy_density_group_integral(energy_groups_boundries[g], energy_groups_boundries[g+1], temperature);
 
         double const nu = energy_groups_centers[g] / units::planck_constant;
@@ -93,10 +94,10 @@ void ComptonMatrixMC::set_Bg_ng(double const temperature){
     }
 }
 
-Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
+Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature) {
 
-    for(std::size_t i=0; i < num_energy_groups; ++i){
-        for(std::size_t j=0; j < num_energy_groups; ++j){
+    for (std::size_t i=0; i < num_energy_groups; ++i) {
+        for (std::size_t j=0; j < num_energy_groups; ++j) {
             S_temp[i][j] = 0.0;
         }
     }
@@ -105,16 +106,15 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
     std::vector<double> const Omega_0(3., 0.);
     std::vector<double> Omega_0_tag(3., 0.), Omega_e(3., 0.), Omega_tag(3., 0.), Omega_p_tag(3., 0.);
     std::vector<double> weight(num_energy_groups, 0.);
-    for(std::size_t sample_i=0; sample_i < num_of_samples; ++sample_i){
+    for (std::size_t sample_i=0; sample_i < num_of_samples; ++sample_i) {
 
-        if((sample_i+1) % (num_of_samples/4) == 0 or sample_i==0){
+        if ((sample_i+1) % (num_of_samples/4) == 0 or sample_i==0) {
             printf("Compton matrix T=%gkev sample %ld/%ld [%d%%]\n", temperature/units::kev_kelvin, sample_i+1, num_of_samples, int(100*double(sample_i+1.)/double(num_of_samples)));
         }
 
-
         // step 1: sample electron velocity from a weighted Maxwell Juttner distribution 
         double const gamma = sample_gamma(temperature);
-        
+
         // weight of sample
         double const beta = std::sqrt(1.0 - 1.0 / (gamma*gamma));
         sum_beta += beta;
@@ -123,10 +123,10 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
         double const mu_e = 1.0 - 2.0*sample_uniform_01();
         Omega_e[0] = std::sqrt(1. - mu_e*mu_e);
         Omega_e[2] = mu_e;
-        
+
         // step 3:
         double const D0 = gamma * (1.0 - beta*mu_e);
-        
+
         // step 4: the direction of the photon before the scattering in the rest frame of the electron
         double const mu_0_tag = 1. / D0 * (1. - gamma/(1.+gamma)*(D0+1.)*beta*mu_e);
         double const sin_0_tag = std::sqrt(1. - mu_0_tag*mu_0_tag);
@@ -148,10 +148,10 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
 
         // step 7 
         double const D_tag = gamma*(1. + beta*(Omega_tag[0]*Omega_e[0] + Omega_tag[2]*Omega_e[2]));
-        
+
         // step 8: sample the energy groups 
         double const interp = sample_uniform_01();
-        for(std::size_t g0=0; g0<num_energy_groups; ++g0){
+        for (std::size_t g0=0; g0<num_energy_groups; ++g0) {
             // step 8a: sample energy
             double const boundry_g0 = energy_groups_boundries[g0];
             double width = energy_groups_boundries[g0+1]-boundry_g0;
@@ -162,7 +162,7 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
             double const w_E0 = (E0*E0)/(boundry_g0*boundry_g0)*std::exp(-a);
 
             weight[g0] += w_E0;
-            
+
             // step 8b: calculate E
             double const E0_tag = D0*E0;
             double const A = 1. / (1. + (1. - mu_p_tag)*E0_tag / units::me_c2);
@@ -178,50 +178,49 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
 
             // step 8d: calcualte the cross section contribution
             double const sigma = 0.75 * D0/gamma * A*A*(A + 1./A - sin_p_tag*sin_p_tag)*w_E0*beta;
-            
-            if(g0 == static_cast<std::size_t>(g)){
+
+            if (g0 == static_cast<std::size_t>(g)) {
                 S_temp[g0][g] += sigma;
             } else {
                 double const fac = (E-E0)/(energy_groups_centers[g]-energy_groups_centers[g0]);
                 S_temp[g0][g] += sigma*fac;
                 S_temp[g0][g0] += sigma*(1.0-fac);
             }
-        }    
+        }
     }
 
     // total weight
     double const beta_avg = sum_beta / num_of_samples;
-    
+
     // multiply by sigma_thomson and normalization factors
-    for(std::size_t g0=0; g0 < num_energy_groups; ++g0){
-        for(std::size_t g=0; g < num_energy_groups; ++g){
+    for (std::size_t g0=0; g0 < num_energy_groups; ++g0) {
+        for (std::size_t g=0; g < num_energy_groups; ++g) {
             double const weight_avg = weight[g0]/num_of_samples;
             S_temp[g0][g] *= units::sigma_thomson/(num_of_samples*beta_avg*weight_avg);
         }
     }
 
-    if(force_detailed_balance){
+    if (force_detailed_balance) {
         set_Bg_ng(temperature);
         double constexpr thresh = units::sigma_thomson*std::numeric_limits<double>::epsilon()*1e3;
 
-        for(std::size_t g=0; g < num_energy_groups; ++g){
+        for (std::size_t g=0; g < num_energy_groups; ++g) {
             double const E_g = energy_groups_centers[g];
-            
-            for(std::size_t gt=g+1; gt<num_energy_groups; ++gt){
-                if(S_temp[gt][g] < thresh and S_temp[g][gt] < thresh) continue;
 
-                if(B[g]<std::numeric_limits<double>::min()*1e40 or B[gt]<std::numeric_limits<double>::min()*1e40){
+            for (std::size_t gt=g+1; gt<num_energy_groups; ++gt) {
+                if (S_temp[gt][g] < thresh and S_temp[g][gt] < thresh) continue;
+
+                if (B[g]<std::numeric_limits<double>::min()*1e40 or B[gt]<std::numeric_limits<double>::min()*1e40) {
                     S_temp[g][gt] = S_temp[gt][g] = 0.;
                     continue;
                 }
 
                 double const E_gt = energy_groups_centers[gt];
                 double const detailed_balance_factor = (1.0+n_eq[gt])*B[g]*E_gt / ((1.0+n_eq[g])*B[gt]*E_g);
-                
-                if(detailed_balance_factor < 1.0){
+
+                if (detailed_balance_factor < 1.0) {
                     S_temp[gt][g] = S_temp[g][gt]*detailed_balance_factor;
-                }
-                else{
+                } else {
                     S_temp[g][gt] = S_temp[gt][g]/detailed_balance_factor;
                 }
             }
@@ -231,16 +230,16 @@ Matrix ComptonMatrixMC::calculate_S_matrix(double const temperature){
     return S_temp;
 }
 
-void ComptonMatrixMC::set_tables(std::vector<double> const& temperature_grid_){
+void ComptonMatrixMC::set_tables(std::vector<double> const& temperature_grid_) {
 
-    if(temperature_grid_.size()<2){
+    if (temperature_grid_.size()<2) {
         printf("Compton temperature grid has less than two temperature points - %ld\n", temperature_grid_.size());
         exit(1);
     }
     printf("setting Compton matrix tables for %ld temperatures (in kev):\n", temperature_grid_.size());
-    for(std::size_t i=0; i<temperature_grid_.size(); ++i){
+    for (std::size_t i=0; i<temperature_grid_.size(); ++i) {
         printf("%g ", temperature_grid_[i]/units::kev_kelvin);
-        if(i>0 and temperature_grid_[i]<=temperature_grid_[i-1]){
+        if (i>0 and temperature_grid_[i]<=temperature_grid_[i-1]) {
             printf("fatal - Compton temperature grid is not monotonic\n");
             exit(1);
         }
@@ -250,89 +249,88 @@ void ComptonMatrixMC::set_tables(std::vector<double> const& temperature_grid_){
     temperature_grid = temperature_grid_;
     S_log_tables = std::vector<Matrix>(temperature_grid.size(), Matrix(num_energy_groups, Vector(num_energy_groups, 0.0)));
     dSdUm_tables = std::vector<Matrix>(temperature_grid.size(), Matrix(num_energy_groups, Vector(num_energy_groups, 0.0)));
-    
-    for(std::size_t i=0; i < temperature_grid.size(); ++i){
+
+    for (std::size_t i=0; i < temperature_grid.size(); ++i) {
         S_log_tables[i] = calculate_S_matrix(temperature_grid[i]);
-        for(std::size_t g0=0; g0 < num_energy_groups; ++g0){
-            for(std::size_t g=0; g < num_energy_groups; ++g){
+        for (std::size_t g0=0; g0 < num_energy_groups; ++g0) {
+            for (std::size_t g=0; g < num_energy_groups; ++g) {
                 S_log_tables[i][g0][g] = std::log(S_log_tables[i][g0][g]);
             }
         }
     }
 
-    for(std::size_t i=0; i+1 < temperature_grid.size(); ++i){
+    for (std::size_t i=0; i+1 < temperature_grid.size(); ++i) {
         using boost::math::pow;
 
         double const T1 = temperature_grid[i];
         double const T2 = temperature_grid[i+1];
-        
+
         double const dUm = units::arad*(pow<4>(T2) - pow<4>(T1));
 
-        for(std::size_t g=0; g < num_energy_groups; ++g){
-            for(std::size_t gt=0; gt<num_energy_groups; ++gt){
+        for (std::size_t g=0; g < num_energy_groups; ++g) {
+            for (std::size_t gt=0; gt<num_energy_groups; ++gt) {
                 dSdUm_tables[i][g][gt] = (std::exp(S_log_tables[i+1][g][gt]) - std::exp(S_log_tables[i][g][gt]))/dUm;
             }
         }
     }
 }
 
-void ComptonMatrixMC::get_tau_matrix(double const temperature, double const density, double const A, double const Z, Matrix& tau, Matrix& dtau_dUm){
+void ComptonMatrixMC::get_tau_matrix(double const temperature, double const density, double const A, double const Z, Matrix& tau, Matrix& dtau_dUm) {
     auto const tmp_iterator = std::lower_bound(temperature_grid.cbegin(), temperature_grid.cend(), temperature);
     auto const tmp_i = std::distance(temperature_grid.cbegin(), tmp_iterator) - 1; //  gives the index of lower bound of the temperature in the temperature grid
 
-    if(tmp_i+1 == static_cast<int>(temperature_grid.size())){
+    if (tmp_i+1 == static_cast<int>(temperature_grid.size())) {
         printf("temperature T=%gkev given to get_tau_matrix is too high (maximal table temperature=%gkev)\n", temperature/units::kev_kelvin, temperature_grid.back()/units::kev_kelvin);
         exit(1);
     }
 
-    if(tmp_i == -1){
+    if (tmp_i == -1) {
         printf("temperature T=%gkev given to get_tau_matrix is too low (minimal table temperature=%gkev)\n", temperature/units::kev_kelvin, temperature_grid[0]/units::kev_kelvin);
         exit(1);
     }
 
-    if(tau.size() != num_energy_groups or dtau_dUm.size() != num_energy_groups){
+    if (tau.size() != num_energy_groups or dtau_dUm.size() != num_energy_groups) {
         std::cout << "tau or dtau_dUm given to get_tau_matrix has less then `num_energy_groups` rows" << std::endl;
         exit(1);
     }
 
-    for(std::size_t g=0; g < num_energy_groups; ++g){
-        if(tau[g].size() != num_energy_groups or dtau_dUm[g].size() != num_energy_groups){
+    for (std::size_t g=0; g < num_energy_groups; ++g) {
+        if (tau[g].size() != num_energy_groups or dtau_dUm[g].size() != num_energy_groups) {
             std::cout << "tau or dtau_dUm given to get_tau_matrix has less then `num_energy_groups` columns" << std::endl;
             exit(1);
         }
     }
 
-    if(force_detailed_balance) set_Bg_ng(temperature);
+    if (force_detailed_balance) set_Bg_ng(temperature);
 
     double const x = (temperature-temperature_grid[tmp_i])/(temperature_grid[tmp_i+1]-temperature_grid[tmp_i]);
-    for(std::size_t i = 0; i < num_energy_groups; ++i){
+    for (std::size_t i = 0; i < num_energy_groups; ++i) {
         double const E_i = energy_groups_centers[i];
 
-        for(std::size_t j=i; j < num_energy_groups; ++j){
+        for (std::size_t j=i; j < num_energy_groups; ++j) {
             tau[i][j] = std::exp(S_log_tables[tmp_i][i][j])*(1. - x) + std::exp(S_log_tables[tmp_i+1][i][j])*x;
-            
-            if(i == j) continue;
-            
+
+            if (i == j) continue;
+
             tau[j][i] = std::exp(S_log_tables[tmp_i][j][i])*(1. - x) + std::exp(S_log_tables[tmp_i+1][j][i])*x;
 
             // enforce detailed balance on the interpolated matrix
-            if(force_detailed_balance){
+            if (force_detailed_balance) {
 
-                if(B[i]<std::numeric_limits<double>::min()*1e40 or B[j]<std::numeric_limits<double>::min()*1e40){
+                if (B[i]<std::numeric_limits<double>::min()*1e40 or B[j]<std::numeric_limits<double>::min()*1e40) {
                     tau[i][j] = tau[j][i] = 0.;
                     continue;
                 }
 
                 double const E_j = energy_groups_centers[j];
                 double const detailed_balance_factor = (1.0+n_eq[j])*B[i]*E_j / ((1.0+n_eq[i])*B[j]*E_i);
-                
-                if(detailed_balance_factor < 1.0) {
+
+                if (detailed_balance_factor < 1.0) {
                     tau[j][i] = tau[i][j]*detailed_balance_factor;
                 } else {
                     tau[i][j] = tau[j][i]/detailed_balance_factor;
                 }
-            } 
-            else{
+            } else {
                 tau[j][i] = std::exp(S_log_tables[tmp_i][j][i])*(1. - x) + std::exp(S_log_tables[tmp_i+1][j][i])*x;
             }
         }
@@ -340,15 +338,15 @@ void ComptonMatrixMC::get_tau_matrix(double const temperature, double const dens
 
     double const Nelectron = density*units::Navogadro/A*Z;
     dtau_dUm = dSdUm_tables[tmp_i];
-    for(std::size_t i=0; i<num_energy_groups; ++i){
-        for(std::size_t j=0; j<num_energy_groups; ++j){
+    for (std::size_t i=0; i<num_energy_groups; ++i) {
+        for (std::size_t j=0; j<num_energy_groups; ++j) {
             tau[i][j] *= Nelectron;
             dtau_dUm[i][j] *= Nelectron;
         }
     }
 }
 
-Matrix ComptonMatrixMC::get_tau_matrix(double const temperature, double const density, double const A, double const Z){
+Matrix ComptonMatrixMC::get_tau_matrix(double const temperature, double const density, double const A, double const Z) {
     Matrix tau(num_energy_groups, Vector(num_energy_groups, 0.0));
     Matrix dtau(num_energy_groups, Vector(num_energy_groups, 0.0));
 
--- src/compton_matrix_mc.hpp
+++ src/compton_matrix_mc.hpp
@@ -7,91 +7,91 @@ using Vector = std::vector<double>;
 using Matrix = std::vector<std::vector<double>>;
 
 class ComptonMatrixMC {
-    public:
+public:
     /**
      * @brief Constructs a new Compton Matrix Monte Carlo object for the calculation of
      * Compton scattering matrix on given photon energy groups.
-     * 
+     *
      * All quantities are always given in c.g.s. units
-     * 
+     *
      * @param energy_groups_centers_ - centers of energy groups [erg] (does not have to be the arithmetic center).
      * @param energy_groups_boundries_ - groups boundaries [erg]
      * @param num_of_samples_ - number of Monet carlo samples for the integration.
      * @param force_detailed_balance_ - whether or not force detailed balance.
      * @param seed_ - if given non-negative value - sets the seed of the random number generator - to enable bit-by-bit reproducible results.
      */
-        ComptonMatrixMC(
-            Vector const energy_groups_centers_, 
-            Vector const energy_groups_boundries_, 
-            std::size_t const num_of_samples_, 
-            bool const force_detailed_balance_,
-            int const seed_=-1);
+    ComptonMatrixMC(
+        Vector const energy_groups_centers_,
+        Vector const energy_groups_boundries_,
+        std::size_t const num_of_samples_,
+        bool const force_detailed_balance_,
+        int const seed_=-1);
 
-        /**
-         * @brief Calculates the *microscopic* Compton scattering matrix at
-         * the given temperature (without interpolation on temperature). 
-         * The calculation is perfoemed using a Monte-Carlo integration.
-         * 
-         * @param temperature the given temeprature [K]
-         * @return Matrix the microscopic Compton scattering matrix [cm^2]
-         */
-        Matrix calculate_S_matrix(double const temperature);
-        
-        /**
-         * @brief Given a set of temperatures, calculates and store the S matrices, so 
-         * that tau matrices can be calculated for other temperatures using interpolation between 
-         * the given temperatures (using the `get_tau_matrix` functions).
-         * @param temperature_grid - a set of temperatures [K]
-         * @return * void 
-         */
-        void set_tables(std::vector<double> const& temperature_grid_);
-        
-        /**
-         * @brief Get the Compton tau matrix - which is the macroscopic cross secion for Compton scattering
-         * The result is calculated for the given temperature by interpolation over the set of temperatures
-         * given to `set_tables`.
-         * 
-         * @param temperature - the given electron temperature [K]
-         * @param density - the given mass density
-         * @param A  - the given atomic wieght
-         * @param Z - the average number of free electrons per nucleous (ionization)
-         * @param tau - the resulting Compton tau marix (units of 1/cm).
-         * @param dtau_dUm the derivative of tau with respect to aT^4
-         */
-        void get_tau_matrix(double const temperature, double const density, double const A, double const Z, Matrix& tau, Matrix& dtau_dUm);
+    /**
+     * @brief Calculates the *microscopic* Compton scattering matrix at
+     * the given temperature (without interpolation on temperature).
+     * The calculation is perfoemed using a Monte-Carlo integration.
+     *
+     * @param temperature the given temeprature [K]
+     * @return Matrix the microscopic Compton scattering matrix [cm^2]
+     */
+    Matrix calculate_S_matrix(double const temperature);
 
-        Matrix get_tau_matrix(double const temperature, double const density, double const A, double const Z);
+    /**
+     * @brief Given a set of temperatures, calculates and store the S matrices, so
+     * that tau matrices can be calculated for other temperatures using interpolation between
+     * the given temperatures (using the `get_tau_matrix` functions).
+     * @param temperature_grid - a set of temperatures [K]
+     * @return * void
+     */
+    void set_tables(std::vector<double> const& temperature_grid_);
 
-       /**
-        * @brief Sample from the distribution A*gamma^2e^(-gamma/theta)
-        * where theta=kB T / m_e * c^2
-        * @param temperature the given temperature [K]
-        * @return double the sampled value of gamma
-        */
-       double sample_gamma(double const temperature);
-    
-    private:
-        void set_Bg_ng(double const);
-        
-        Vector const energy_groups_centers;
-        Vector const energy_groups_boundries;
-        std::size_t const num_energy_groups;
-        
-        std::size_t const num_of_samples;
-        unsigned int const seed;
-        boost::random::variate_generator<boost::random::mt19937, boost::random::uniform_01<>> sample_uniform_01;
+    /**
+     * @brief Get the Compton tau matrix - which is the macroscopic cross secion for Compton scattering
+     * The result is calculated for the given temperature by interpolation over the set of temperatures
+     * given to `set_tables`.
+     *
+     * @param temperature - the given electron temperature [K]
+     * @param density - the given mass density
+     * @param A  - the given atomic wieght
+     * @param Z - the average number of free electrons per nucleous (ionization)
+     * @param tau - the resulting Compton tau marix (units of 1/cm).
+     * @param dtau_dUm the derivative of tau with respect to aT^4
+     */
+    void get_tau_matrix(double const temperature, double const density, double const A, double const Z, Matrix& tau, Matrix& dtau_dUm);
 
-        bool const force_detailed_balance;
+    Matrix get_tau_matrix(double const temperature, double const density, double const A, double const Z);
 
-        // tabulation
-        Vector temperature_grid;
-        std::vector<Matrix> S_log_tables;
-        std::vector<Matrix> dSdUm_tables;
+    /**
+     * @brief Sample from the distribution A*gamma^2e^(-gamma/theta)
+     * where theta=kB T / m_e * c^2
+     * @param temperature the given temperature [K]
+     * @return double the sampled value of gamma
+     */
+    double sample_gamma(double const temperature);
 
-        // auxiliary arrays
-        Matrix S_temp;
-        std::vector<double> n_eq;
-        std::vector<double> B;
+private:
+    void set_Bg_ng(double const);
+
+    Vector const energy_groups_centers;
+    Vector const energy_groups_boundries;
+    std::size_t const num_energy_groups;
+
+    std::size_t const num_of_samples;
+    unsigned int const seed;
+    boost::random::variate_generator<boost::random::mt19937, boost::random::uniform_01<>> sample_uniform_01;
+
+    bool const force_detailed_balance;
+
+    // tabulation
+    Vector temperature_grid;
+    std::vector<Matrix> S_log_tables;
+    std::vector<Matrix> dSdUm_tables;
+
+    // auxiliary arrays
+    Matrix S_temp;
+    std::vector<double> n_eq;
+    std::vector<double> B;
 };
 
 #endif
